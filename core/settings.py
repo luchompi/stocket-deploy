bc7241516ebf6cdc8ccf8b706e5ab3175fccdb0f
luchompi <lpichon63@gmail.com>
2023-09-12 19:47:28 -0500

    Se ha corregido configuracion segun indica bard

--- core/settings.py
+++ core/settings.py
@@ -203,10 +203,6 @@ DJOSER = {
 AUTH_USER_MODEL = 'accounts.UserAccount'
 
 ### Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_BACKEND = "mailersend.backends.MailerSendBackend"
+MAILERSEND_API_KEY = "YOUR_API_KEY"
+MAILERSEND_DOMAIN = "YOUR_DOMAIN"
